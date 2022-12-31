cd7580ee321fbf1c4dbd97fb27c9cfa77e0345a4
Josep Gisbert <gisbertjosep@gmail.com>
2022-12-31 09:23:11 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pisos-santcugat',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
